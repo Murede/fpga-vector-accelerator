4bdbf71adfa6748c3824185066ac775d24a1764b
Murede <Murede2005@gmail.com>
2026-09-01 14:22:55 -0400

    FInished Signed MAC Unit, developing the testbench now

--- rtl/signed_mac_unit.sv
+++ rtl/signed_mac_unit.sv
@@ -1,15 +1,24 @@
 module signed_mac_unit(
-    inpput logic clk,
+    input logic clk,
     input logic reset,
     input logic en,
 
     input logic signed [7:0] a,
     input logic signed [7:0] b,
 
-    inpit logic signed [17:0] accumulator
+    output logic signed [17:0] accumulator
 
 );
 
     logic signed [15:0] product;
+
+    assign product = a * b;
+
+    always_ff @(posedge clk) begin 
+        if (reset)
+            accumulator <= 18'sd0;
+        else if (en)
+            accumulator <= accumulator + {{2{product[15]}}, product}; 
+    end 
 
 endmodule
