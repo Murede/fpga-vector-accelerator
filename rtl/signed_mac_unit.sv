8df60c2832604ecc047da926a6a50aaa909198e1
Murede <Murede2005@gmail.com>
2026-09-01 14:11:07 -0400

    Satrting the signed mac unit RTL development

---
+++ rtl/signed_mac_unit.sv
@@ -0,0 +1,15 @@
+module signed_mac_unit(
+    inpput logic clk,
+    input logic reset,
+    input logic en,
+
+    input logic signed [7:0] a,
+    input logic signed [7:0] b,
+
+    inpit logic signed [17:0] accumulator
+
+);
+
+    logic signed [15:0] product;
+
+endmodule
