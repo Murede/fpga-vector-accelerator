9dc203d8b159f60ec945febd8862e9ca238a3d24
Murede <Murede2005@gmail.com>
2026-09-04 13:49:57 -0400

    Completed the top module for the vector accelerator

--- rtl/topmodule.sv
+++ rtl/topmodule.sv
@@ -1,14 +1,17 @@
 module topmodule #(
     parameter int DATA_WIDTH = 8,
     parameter int VECTOR_LEN = 4,
-    parameter nt NUM_ROWS = 4,
-    paramerer int ACC_WIDTH =
+    parameter int NUM_ROWS = 4,
+    parameter int ACC_WIDTH =
         (2 * DATA_WIDTH) + $clog2(VECTOR_LEN)
 ) (
     // Control Inputs 
+
     input logic clk,
     input logic reset,
     input logic start,
+
+    // Data Inputs 
 
     input logic signed [DATA_WIDTH-1:0] a
         [NUM_ROWS-1:0][VECTOR_LEN-1:0],
@@ -16,6 +19,28 @@ module topmodule #(
     input logic signed [DATA_WIDTH-1:0] x
         [VECTOR_LEN-1:0],
 
-    output logic signed [DATA_WIDTH-1:0] y
+    // Data Output 
+    output logic signed [ACC_WIDTH-1:0] y
         [NUM_ROWS-1:0],
-)
+    
+    // Control Output 
+    output logic done
+);
+
+    // Parallel Matrix Multiplier Instatiation 
+
+    parallel_matrix_multiplier # (
+        .DATA_WIDTH(DATA_WIDTH),
+        .VECTOR_LEN(VECTOR_LEN),
+        .NUM_ROWS(NUM_ROWS),
+        .ACC_WIDTH(ACC_WIDTH)
+    ) parallel_mm_unit (
+        .clk(clk),
+        .reset(reset),
+        .start(start),
+        .done(done),
+        .a(a),
+        .x(x),
+        .y(y),
+    );
+endmodule
